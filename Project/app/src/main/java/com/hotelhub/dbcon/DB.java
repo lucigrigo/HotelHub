8728fbe3b0dd6ee3e039b9e113d704df02984dc1
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-08 21:48:55 +0300

    installed android studio on laptop and checked if it works

--- Project/app/src/main/java/com/hotelhub/dbcon/DB.java
+++ Project/app/src/main/java/com/hotelhub/dbcon/DB.java
@@ -1,9 +1,7 @@
 package com.hotelhub.dbcon;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
 class Main {
     public static void main(String[] args) {
-
+        System.out.println("hi");
     }
 }
