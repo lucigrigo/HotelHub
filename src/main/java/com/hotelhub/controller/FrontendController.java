238f0aa1886080f435f3b0be640c732ee95b663d
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-09 15:30:41 +0300

    created connection between UserServices and FrontendController

--- src/main/java/com/hotelhub/controller/FrontendController.java
+++ src/main/java/com/hotelhub/controller/FrontendController.java
@@ -1,18 +1,27 @@
 package com.hotelhub.controller;
 
+import com.hotelhub.model.User;
+import com.hotelhub.services.UserServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FrontendController {
     @PostMapping("/users/create")
-    public ResponseEntity<Object> createUser() {
-        return null;
+    public ResponseEntity<Object> createUser(
+            @RequestBody User user
+    ) {
+        return UserServices.createUser(user);
     }
 
     @PostMapping("/users/login")
-    public ResponseEntity<Object> loginUser() {
-        return null;
+    public ResponseEntity<Object> loginUser(
+            @RequestParam(required = false, name = "email") String email,
+            @RequestParam(required = false, name = "password") String password
+    ) {
+        return UserServices.loginUser(email, password);
     }
 }
--- src/main/java/com/hotelhub/services/UserServices.java
+++ src/main/java/com/hotelhub/services/UserServices.java
@@ -1,4 +1,17 @@
 package com.hotelhub.services;
 
+import com.hotelhub.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class UserServices {
+    public static ResponseEntity<Object> createUser(User newUser) {
+        // TODO: add user to database; check if he already exists
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public static ResponseEntity<Object> loginUser(String email, String password) {
+        // TODO: check if user exists in database
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
