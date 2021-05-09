38e37916d23b870470470c63bbe2151409352c4e
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-09 13:22:31 +0300

    Create HotelHubApplication.java

---
+++ src/main/java/com/hotelhub/HotelHubApplication.java
@@ -0,0 +1,11 @@
+package com.hotelhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelHubApplication.class, args);
+    }
+}
