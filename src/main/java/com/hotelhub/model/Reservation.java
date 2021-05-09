26f4c1f003f5dfe6d57d0a861e59cf5ae5dbd879
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-09 14:05:41 +0300

    created Entity annotations and added todo text file

--- src/main/java/com/hotelhub/model/Reservation.java
+++ src/main/java/com/hotelhub/model/Reservation.java
@@ -1,13 +1,26 @@
 package com.hotelhub.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id_reservation;
+
     private String room;
+
     private int room_id;
+
     private long user_id;
+
     private boolean approved;
+
     private Date date_start;
+
     private Date date_end;
 }
