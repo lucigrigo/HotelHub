7040aee3408c4868672d8dd7254da402640c62cb
Grigore Lucian <lucigrigo@yahoo.com>
2021-05-09 13:29:35 +0300

    Update FrontendController.java

--- src/main/java/com/hotelhub/controller/FrontendController.java
+++ src/main/java/com/hotelhub/controller/FrontendController.java
@@ -1,8 +1,18 @@
 package com.hotelhub.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FrontendController {
+    @PostMapping("/users/create")
+    public ResponseEntity<Object> createUser() {
 
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<Object> loginUser() {
+
+    }
 }
