a4fe5d65020066ba5594b37fb09ff558b002eb6c
ediolteanu99 <ediolteanu99@yahoo.com>
2021-05-10 21:37:49 +0300

    implemented logic for login and create user

--- src/main/java/com/hotelhub/controller/DatabaseController.java
+++ src/main/java/com/hotelhub/controller/DatabaseController.java
@@ -3,6 +3,7 @@ package com.hotelhub.controller;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
+import com.hotelhub.model.User;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.auth.oauth2.GoogleCredentials;
 
@@ -19,29 +20,54 @@ import java.util.concurrent.ExecutionException;
 
 @RestController
 public class DatabaseController {
-    private static int no_users;
-    // Use a service account
-    private static Firestore initDBConnection() throws IOException {
+    private static Firestore database = null;
+    public static Firestore getDatabase() throws IOException {
+        if (database != null) {
+            return database;
+        }
         InputStream serviceAccount = new FileInputStream("key.json");
         GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(credentials)
                 .build();
         FirebaseApp.initializeApp(options);
+        database = FirestoreClient.getFirestore();
+        return database;
+    }
 
-        return FirestoreClient.getFirestore();
+    public static boolean searchUser(Firestore db, String email) throws ExecutionException, InterruptedException {
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            if (document.getString("email").equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkCredentials(Firestore db, String email, String password) throws ExecutionException, InterruptedException {
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            if (document.getString("email").equals(email)) {
+                if (document.getString("password").equals(password)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 
     public static void read(Firestore db) throws ExecutionException, InterruptedException {
-        // asynchronously retrieve all users
         ApiFuture<QuerySnapshot> query = db.collection("users").get();
-        // ...
-        // query.get() blocks on response
         QuerySnapshot querySnapshot = query.get();
         List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
-        int i = 0;
         for (QueryDocumentSnapshot document : documents) {
-            i++;
             System.out.println("User: " + document.getId());
             System.out.println("email: " + document.getString("email"));
             System.out.println("password: " + document.getString("password"));
@@ -50,27 +76,23 @@ public class DatabaseController {
                 System.out.println("hotel_admin: " + document.getLong("hotel_admin"));
             }
         }
-        no_users = i;
     }
 
-    public static void addUser(Firestore db, String email, String password, boolean is_admin, int hotel_admin)
+    public static void addUser(Firestore db, User newUser)
             throws ExecutionException, InterruptedException {
-        DocumentReference docRef = db.collection("users").document(Integer.toString(no_users + 1));
-        // Add document data with an additional field ("middle")
+        DocumentReference docRef = db.collection("users").document(Integer.toString(newUser.getId_user()));
         Map<String, Object> data = new HashMap<>();
-        data.put("email", email);
-        data.put("password", password);
-        data.put("is_admin", is_admin);
-        if (is_admin) {
-            data.put("hotel_admin", hotel_admin);
+        data.put("email", newUser.getEmail());
+        data.put("password", newUser.getPassword());
+        data.put("is_admin", newUser.isAdmin());
+        if (newUser.isAdmin()) {
+            data.put("hotel_admin", newUser.getHotel_admin());
         }
         ApiFuture<WriteResult> result = docRef.set(data);
         System.out.println("Update time : " + result.get().getUpdateTime());
     }
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        Firestore db = initDBConnection();
-        read(db);
-        //addUser(db, "test3@email.com", "test3", false, 0);
+    public static void main(String[] args) {
+
     }
 }
