ac440d84423d575a3636447d23d5c7313f959855
ediolteanu99 <ediolteanu99@yahoo.com>
2021-05-11 00:31:38 +0300

    optimized imports

--- src/main/java/com/hotelhub/controller/FrontendController.java
+++ src/main/java/com/hotelhub/controller/FrontendController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-
 @RestController
 public class FrontendController {
     @PostMapping("/users/create")
