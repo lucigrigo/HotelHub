ed170bb40b593ffea7036919531c639805705c85
AndraVasilache <vasilacheandragabriela@gmail.com>
2021-05-13 01:26:48 +0300

    crossorigin

--- src/main/java/com/hotelhub/controller/FrontendController.java
+++ src/main/java/com/hotelhub/controller/FrontendController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:19007/" )
+@CrossOrigin
 public class FrontendController {
 
     @PostMapping("/users/create")
